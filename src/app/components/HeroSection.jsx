97cb3a238688d15c1b60d15dee0aeb90c91d9628
Nishant-chaudhari-7 <140783494+Nishant-chaudhari-7@users.noreply.github.com>
2024-06-16 14:45:51 +0530

    Update HeroSection.jsx

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -50,7 +50,7 @@ const HeroSection = () => {
               Hire Me
             </Link>
             <Link
-              href="https://drive.google.com/uc?export=download&id=1E1KXW9esON8UhU_6AHxDDceSmM95CBQz"
+              href="https://drive.google.com/file/d/15nehiqbYisR5u8XePzNTcUQSdOHQ8WXL/view?usp=drive_link"
               download="My-CV"
               target="_blank"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
